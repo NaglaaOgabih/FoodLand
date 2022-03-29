65b22c802565e76d651edece4a325dc7ae975104
Naglaa Ogabih <naglaaogabih@Naglaas-MacBook-Pro.local>
2022-03-29 09:40:33 +0200

    edit

--- FoodLand/Controller/SlideUpViewController.swift
+++ FoodLand/Controller/SlideUpViewController.swift
@@ -8,36 +8,19 @@
 import UIKit
 
 
-<<<<<<< HEAD
 class SlideUpViewController: UIViewController {
     
     @IBOutlet weak var bottomAnchor: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }    
-    @IBAction func menuBtnPressed(_ sender: Any) {
 
-    }
-//    func didTabCloseBtn() {
-//        bottomAnchor.constant = -350
-//        UIView.animate(withDuration: 0.3){
-//            self.view.layoutIfNeeded()
-//        }
-//    }
-=======
-class SlideUpViewController: UIViewController, colseBtnSettingsDelegate {
-    var menuTableViewController : MenuTableViewController?
+    //    class SlideUpViewController: UIViewController {
+    //    var menuTableViewController : MenuTableViewController?
     
-    @IBOutlet weak var bottomAnchor: NSLayoutConstraint!
-    var isMenuShowing = false
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        menuTableViewController?.delegate = self
-
-    }    
     @IBAction func menuBtnPressed(_ sender: Any) {
-        menuTableViewController?.delegate = self
+        //        menuTableViewController?.delegate = self
         bottomAnchor.constant = 0
         UIView.animate(withDuration: 0.3){
             self.view.layoutIfNeeded()
@@ -49,5 +32,5 @@ class SlideUpViewController: UIViewController, colseBtnSettingsDelegate {
             self.view.layoutIfNeeded()
         }
     }
->>>>>>> 7cb3234f1cdfae831015650759ce251f11123873
 }
+//}
